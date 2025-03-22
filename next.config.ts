f71b7509a150de9c5df5853d6ddcf5098936827b
KaiaWalters <kaia.cwalters@gmail.com>
2025-03-22 17:05:10 -0400

    Enables static exports and update build settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output:'export', //enables static exports, the site atm is a SPA (single page web app)
 };
 
 export default nextConfig;
