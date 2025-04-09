707e9494eb67d860baaeb0a4bd7568aee7f2ebf6
KaiaWalters <kaia.cwalters@gmail.com>
2025-04-08 22:41:19 -0400

    adds animation to navbar

--- bigwinfitness2/frontend/src/components/Navbar.tsx
+++ bigwinfitness2/frontend/src/components/Navbar.tsx
@@ -13,7 +13,6 @@ const Navbar = () => {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
-              <img className="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
@@ -25,7 +24,7 @@ const Navbar = () => {
           <Notifications />
         </div>
       </div>
-      <MobileMenu menuOpen={menuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
     </nav>
   );
 };
@@ -35,12 +34,11 @@ const HamburgerMenu = ({ menuOpen, setMenuOpen }) => {
     <button
       type="button"
       onClick={() => setMenuOpen(!menuOpen)}
-      className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-800 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset"
+      className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-800 hover:bg-gray-700 hover:text-white focus:outline-none"
       aria-controls="mobile-menu"
       aria-expanded={menuOpen}
     >
-      <span className="sr-only">Toggle main menu</span>
-\      {!menuOpen ? (
+    {!menuOpen ? (
         <svg className="block size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
@@ -48,17 +46,24 @@ const HamburgerMenu = ({ menuOpen, setMenuOpen }) => {
         <svg className="block size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
-      )}
+    )}
     </button>
   );
 };
 
-const MobileMenu = ({ menuOpen }) => {
+const MobileMenu = ({ menuOpen, setMenuOpen }) => {
   return (
-    <div className={`sm:hidden transition-all duration-300 ease-in-out ${menuOpen ? 'block' : 'hidden'}`} id="mobile-menu">
+    <div className={`
+      sm:hidden fixed top-16 left-0 w-full bg-amber-400 z-40
+      transition-all duration-300 ease-in-out
+      transform ${menuOpen ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-full pointer-events-none'}
+      py-8 mt-3
+    `}
+    id="mobile-menu"
+    >
       <div className="space-y-1 px-2 pt-2 pb-3">
-        <Link to="/" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</Link>
-        <Link to="/events" className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-700 hover:text-white">Events</Link>
+        <Link to="/" onClick={()=>{setMenuOpen(!menuOpen)}} className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</Link>
+        <Link to="/events" onClick={()=>{setMenuOpen(!menuOpen)}} className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-700 hover:text-white">Events</Link>
       </div>
     </div>
   );
@@ -67,12 +72,13 @@ const MobileMenu = ({ menuOpen }) => {
 const Notifications = () => {
   return (
     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-      <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden">
+      <button className="px-4 py-2 border-2 rounded-2xl">Login</button>
+      {/* <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden">
         <span className="sr-only">View notifications</span>
         <svg className="size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
         </svg>
-      </button>
+      </button> */}
     </div>
   );
 };
